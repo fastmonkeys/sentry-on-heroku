f13b46df4ea16700e062e9ecd7ac6ca56a4df57e
Janne Vanhala <janne.vanhala@gmail.com>
2012-02-04 20:23:18 +0200

    Added logging configuration.

--- sentry.conf.py
+++ sentry.conf.py
@@ -13,25 +13,73 @@ ROOT = os.path.dirname(__file__)
 
 urlparse.uses_netloc.append('postgres')
 urlparse.uses_netloc.append('mysql')
-try:
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-        engines = {
-            'postgres': 'django.db.backends.postgresql_psycopg2',
-            'mysql': 'django.db.backends.mysql',
+
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+engines = {
+    'postgres': 'django.db.backends.postgresql_psycopg2',
+    'mysql': 'django.db.backends.mysql',
+}
+DATABASES = {
+    'default': {
+        'ENGINE': engines[url.scheme],
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
+}
+
+
+# Logging
+# -------
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         }
-        DATABASES = {
-            'default': {
-                'ENGINE': engines[url.scheme],
-                'NAME': url.path[1:],
-                'USER': url.username,
-                'PASSWORD': url.password,
-                'HOST': url.hostname,
-                'PORT': url.port,
-            }
-        }
-except:
-    print "Unexpected error:", sys.exc_info()
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'south': {
+            'handlers': ['null'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+    },
+}
 
 
 # Sentry configuration
