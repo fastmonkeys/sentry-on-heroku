d8d155b6c2080dfaf72484f271e1f9e26e083037
Janne Vanhala <janne.vanhala@gmail.com>
2013-08-02 14:12:05 +0300

    Fixed email configuration to work with Sentry 6.0.0

--- sentry.conf.py
+++ sentry.conf.py
@@ -49,10 +49,10 @@ SENTRY_ADMINS = os.environ.get('ADMINS', '').split(',')
 SENTRY_MAIL_LEVEL = logging.WARNING
 
 # The prefix to apply to outgoing emails.
-SENTRY_EMAIL_SUBJECT_PREFIX = '[Sentry] '
+EMAIL_SUBJECT_PREFIX = '[Sentry] '
 
 # The reply-to email address for outgoing mail.
-SENTRY_SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'root@localhost')
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'root@localhost')
 
 
 # Security
